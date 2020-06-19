12aaf372b65c071328e0aa39dddb379018424342
Arvin <ariashtary@gmail.com>
2020-06-19 12:30:56 +0200

    🛩️ Small logical improvements

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
-const Ago = timestamp => {
-	const periods = ['sec', 'min', 'hour', 'day', 'week', 'month', 'year', 'century'];
+/**
+ * Convert UNIX timestamp to "time" ago
+ * @param {int} timestamp
+ * @returns {string}
+ */
+export default timestamp => {
+	const periods = ['sec', 'min', 'hour', 'day', 'week', 'month', 'year'];
 	const lengths = [60, 60, 24, 7, 4.35, 12, 10];
 
 	const now = new Date().getTime();
-	let diff = Math.floor(now / 1000) - timestamp;
+	let diff = Math.ceil(now / 1000) - timestamp;
 
 	let i;
-	const _forLength = lengths.length - 1;
+	const _count = lengths.length - 1;
 
-	for (i = 0; diff >= lengths[i] && i < _forLength; i++) {
+	for (i = 0; diff >= lengths[i] && i < _count; i++) {
 		diff /= lengths[i];
 	}
 
-	diff = Math.round(diff);
+	diff = Math.ceil(diff);
 
 	if (diff > 1) {
 		periods[i] += 's';
-	} else if (diff < 0) {
-		diff = 1;
 	}
 
 	return `${diff} ${periods[i]} ago`;
 };
-
-export default Ago;
